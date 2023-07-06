d9129202f7012c5d1fea86e52a5a092c93346614
Amandathah <tmbambo667@gmail.com>
2023-07-06 11:17:39 +0200

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
 		_putchar(*s);
-		return (s + 1);
+		return;
 	}
-	else
-	_putchar('\n');
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
