585cda331ac1118460c8fabd7d957e6ace6c30d4
Amandathah <tmbambo667@gmail.com>
2023-07-07 18:58:20 +0200

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main - A program that print a line using the printf function
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of and int: %d byte(s)n", sizeof(int));
-printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
-return (0);
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+		return (0);
 }
 
